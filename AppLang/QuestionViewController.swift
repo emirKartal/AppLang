61acc4aedbc525d654f86d9028a889b4394cd5a9
emir_kartal <kartal.emirr@gmail.com>
2017-04-25 20:42:15 +0300

    Question Page added
    Question and Result page was added

---
+++ AppLang/QuestionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuestionViewController.swift
+//  AppLang
+//
+//  Created by Emir Kartal on 25.04.2017.
+//  Copyright © 2017 Emir Kartal. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
