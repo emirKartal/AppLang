3aaaa29cea301101de5c35014ebb4d106e3279a3
Çağdaş Ekşi <cagdas.eksi@hotmail.com>
2017-05-20 10:24:19 +0300

    update

--- AppLang/LoginViewController.swift
+++ AppLang/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class LoginViewController: UIViewController {
 
@@ -15,14 +17,93 @@ class LoginViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
+    @IBOutlet weak var lblEmail: UITextField!
+    @IBOutlet weak var lblPassword: UITextField!
+    @IBOutlet weak var labelMessage: UILabel!
+    
+    var json:JSON = []
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
     }
     
+    @IBAction func loginButton(_ sender: Any) {
+        
+        let email = lblEmail.text!
+        let password = lblPassword.text!
+        let deviceId =  UIDevice.current.identifierForVendor!.uuidString
+        print(deviceId)
+        getJSON(userPassword: password, userEmail: email, deviceId:deviceId)
+        
+        
+    }
 
+    func getJSON(userPassword : String, userEmail : String, deviceId: String) {
+        
+        let url = "http://giflisozluk.com/api/v1/student"
+        
+        let params: Parameters = [
+            "Password": userPassword,
+            "email": userEmail
+        ]
+        
+        let headers: HTTPHeaders = [
+            "auth-token": deviceId
+        ]
+        
+        Alamofire.request(url ,method: .post ,parameters: params, encoding: URLEncoding.default, headers: headers).responseJSON { response in
+            
+            switch(response.result) {
+            case .success(_):
+                if let data = response.result.value{
+                    
+                    let json = JSON(data)
+                    
+                    print(json)
+                    print(json["user"])
+                    
+                    let user = json["user"]
+                    
+                    let status = json["status"].intValue
+                    let message = json["message"].stringValue
+                    
+                    let text = " message \(json["message"].stringValue) , status \(json["status"].intValue)"
+                    
+                    print(text)
+                    
+                    UserDefaults.standard.set(status, forKey: "status")
+                    
+                    if status == 200
+                    {
+                        //store data
+                        UserDefaults.standard.set(user["FullName"].stringValue, forKey: "userEmail")
+                        UserDefaults.standard.set(user["token"].stringValue, forKey: "userPassword")
+                        UserDefaults.standard.set(true, forKey: "isUserLoggenIn") //Bool
+                        
+                        self.performSegue(withIdentifier: "homeViewSegue", sender: self)
+                        
+                        
+                    }else if status == 400
+                    {
+                        
+                        self.labelMessage.text = message
+                        
+                    }
+                    
+                }
+                break
+                
+            case .failure(_):
+                print(response.result.error ?? "selam")
+                break
+                
+            }
+            
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
