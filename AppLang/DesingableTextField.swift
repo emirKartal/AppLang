f02eb5fa57d6ec00d24df5112afbbbe8217dead4
Çağdaş Ekşi <cagdas.eksi@hotmail.com>
2017-05-28 11:08:25 +0300

    Dynamic Desingable TextField

---
+++ AppLang/DesingableTextField.swift
@@ -0,0 +1,95 @@
+//
+//  DesingableTextField.swift
+//  AppLang
+//
+//  Created by cagdas on 28/05/2017.
+//  Copyright © 2017 Emir Kartal. All rights reserved.
+//
+
+import UIKit
+
+
+@IBDesignable
+class DesingableTextField: UITextField {
+
+    @IBInspectable var leftImage: UIImage? {
+    
+        didSet {
+            updateView()
+        }
+        
+    }
+
+    @IBInspectable var leftPadding: CGFloat = 0 {
+        
+        didSet {
+            updateView()
+        }
+        
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        
+        didSet {
+            layer.cornerRadius = cornerRadius
+        }
+        
+    }
+    
+    @IBInspectable var imageWidth: CGFloat = 20 {
+        
+        didSet {
+            updateView()
+        }
+        
+    }
+    
+    @IBInspectable var imageHeight: CGFloat = 20 {
+        
+        didSet {
+            updateView()
+        }
+        
+    }
+    
+    func updateView() {
+        
+        if let image = leftImage {
+            leftViewMode = .always
+            
+            let imageView = UIImageView(frame: CGRect(x: leftPadding, y: 0, width: imageWidth, height: imageHeight))
+            
+            imageView.image = image
+            imageView.tintColor = tintColor
+            
+            var width = leftPadding + 20
+            
+            if borderStyle == UITextBorderStyle.none || borderStyle == UITextBorderStyle.line {
+            
+                width = width + 5
+                
+            }
+            
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: width, height: imageHeight))
+            
+            view.addSubview(imageView)
+            
+            leftView = view
+            
+        } else {
+        
+            leftViewMode = .never
+        
+        }
+        
+        attributedPlaceholder = NSAttributedString(string: placeholder != nil ? placeholder! : "", attributes: [NSForegroundColorAttributeName: tintColor])
+        
+    }
+}
+
+
+
+
+
+
+
