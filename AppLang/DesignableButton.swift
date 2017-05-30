bf124533f5d9e976b42090063056e1330dce0b81
Çağdaş Ekşi <cagdas.eksi@hotmail.com>
2017-05-30 22:22:39 +0300

    Designable ui button

---
+++ AppLang/DesignableButton.swift
@@ -0,0 +1,31 @@
+//
+//  DesignableButton.swift
+//  AppLang
+//
+//  Created by cagdas on 30/05/2017.
+//  Copyright © 2017 Emir Kartal. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderWidth: CGFloat = 0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+}
