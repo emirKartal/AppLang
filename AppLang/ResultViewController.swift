372a16fab42f615fc881e5dd093329268f48ad85
emir_kartal <kartal.emirr@gmail.com>
2017-05-31 23:50:02 +0300

    resultTableView added

--- AppLang/ResultViewController.swift
+++ AppLang/ResultViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 
-class ResultViewController: UIViewController {
+class ResultViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.resultTableView.dataSource = self
+        self.resultTableView.dataSource = self
         // Do any additional setup after loading the view.
         
         // Cevaplanamayan sorular listelenecek ve kullanici ister ise tekrar bu sorulara bakabilecek. 
@@ -26,8 +28,21 @@ class ResultViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBOutlet weak var resultTableView: UITableView!
     
-    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = "test"
+        return cell!
+    }
+
+
     /*
     // MARK: - Navigation
 
